7e9bcfc8025dfc2f1ef30d70d746aa7bb2f9c326
Carlos Arturo Rebollo Campos <Carlos Arturo Rebollo Campos>
2017-01-31 21:42:36 -0600

    carga de todos los documentos

---
+++ app/src/main/java/mx/willys/defvb13/MainActivity.java
@@ -0,0 +1,16 @@
+package mx.willys.defvb13;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+//TODO
+}
